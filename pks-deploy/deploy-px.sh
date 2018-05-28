4c753715ddd0fd160b1dcc459f13ad235db909da
Francois Martel <fmrtl73@gmail.com>
2018-05-28 19:58:56 -0400

    fixed envinronment variables for container

--- pks-deploy/deploy-px.sh
+++ pks-deploy/deploy-px.sh
@@ -3,10 +3,7 @@
 set -e
 set -x
 
-export PKS_HOST=https://uaa.opsman.pie-pks-03.cfplatformeng.com:9021
-export PKS_USERNAME=chicago
-export PKS_PASSWORD=2018
-export PKS_CLUSTER_NAME=px-1
+echo `env`
 # Use PKS to connect kubectl to the named cluster
 #
 pks login --api "$PKS_HOST" --username "$PKS_USERNAME" --password "$PKS_PASSWORD" --skip-ssl-verification # FIXME --ca-cert /path/to/cert
