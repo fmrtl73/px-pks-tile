704b2c2e8a0300f393c8fc0c9d694a93ac6ece09
Francois Martel <fmrtl73@gmail.com>
2018-05-30 13:40:58 -0400

    iterate

--- pks-deploy/deploy-px.sh
+++ pks-deploy/deploy-px.sh
@@ -3,14 +3,13 @@
 set -e
 set -x
 
-echo `env`
 # Use PKS to connect kubectl to the named cluster
 #
-pks login --api "$PKS_HOST" --username "$PKS_USERNAME" --password "$PKS_PASSWORD" --skip-ssl-verification # FIXME --ca-cert /path/to/cert
+#pks login --api "$pks_api_uri" --username "$PKS_USERNAME" --password "$PKS_PASSWORD" --skip-ssl-verification # FIXME --ca-cert /path/to/cert
 
 echo "environment: "
 echo `env`
-pks get-credentials "$PKS_CLUSTER_NAME"
+#pks get-credentials "$PKS_CLUSTER_NAME"
 while [[ true ]]; do
   echo "hello world"
   sleep 10
